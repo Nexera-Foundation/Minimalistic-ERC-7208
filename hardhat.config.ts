2105758266ccd6c0e0dfd227afd972d8df587f6f
Pavel Rubin <pash7ka@gmail.com>
2024-11-27 15:12:01 +0100

    Add optimizer runs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -22,6 +22,7 @@ const config: HardhatUserConfig = {
         settings: {
           optimizer: {
             enabled: true,
+            runs: 10000,
           },
           viaIR: true,
         },
